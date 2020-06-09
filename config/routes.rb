070e92c9005f9c2532c467e4435139b9964670a6
stamander <iida.atsushi.fh@gmail.com>
2020-06-09 19:22:57 +0900

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'top#index' 
+  resources :top,only: :index
+  resources :message,only: :index
+  resources :shop, only: [:index,:show] 
+  resources :link, only: :index
 end
