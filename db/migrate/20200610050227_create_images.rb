54a27bf04a7c82acba65605075260bfe9db7826f
stamander <iida.atsushi.fh@gmail.com>
2020-06-10 14:03:39 +0900

    Update 20200610050227_create_images.rb

--- db/migrate/20200610050227_create_images.rb
+++ db/migrate/20200610050227_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
+      t.string :src,null: false
+      t.references :shops, foreign_key: true
 
       t.timestamps
     end
