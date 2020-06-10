d89bcfea1ad98eb9ea13889a03a2142d13117a7d
stamander <iida.atsushi.fh@gmail.com>
2020-06-10 14:12:25 +0900

    new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'top#index' 
   resources :top,only: :index
   resources :message,only: :index
-  resources :shop, only: [:index, :new, :show] 
+  resources :shop
   resources :link, only: :index
 end
