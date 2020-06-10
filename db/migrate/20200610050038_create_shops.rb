7b6da943222ef3d090ee5f15d63194317e5d6949
stamander <iida.atsushi.fh@gmail.com>
2020-06-10 14:01:48 +0900

    model

---
+++ db/migrate/20200610050038_create_shops.rb
@@ -0,0 +1,12 @@
+class CreateShops < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shops do |t|
+      t.string :name,null: false
+      t.string :kana,null: false
+      t.string :phone_number,null: false
+      t.string :mail,null: false
+
+      t.timestamps
+    end
+  end
+end
