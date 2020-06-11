63f60a5d7950e00a5b795af572a1964329bd77a7
stamander <iida.atsushi.fh@gmail.com>
2020-06-11 16:16:01 +0900

    Update exhibition.rb

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,5 +1,5 @@
 class Exhibition < ApplicationRecord
-  has_many :images, dependent: :destroy
+  has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :name, presence: true
   validates :price, presence: true 
