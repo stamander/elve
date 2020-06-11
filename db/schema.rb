c341dbf1806988079211ef1cd917ed0ab14a61f9
stamander <iida.atsushi.fh@gmail.com>
2020-06-11 15:50:45 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_052123) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "exhibitions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.string "explain", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "src", null: false
-    t.bigint "shops_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shops_id"], name: "index_images_on_shops_id"
-  end
-
-  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "kana", null: false
-    t.string "phone_number", null: false
-    t.string "mail", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "images", "shops", column: "shops_id"
 end
