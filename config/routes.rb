9e27137855f5208fab1aadc99ceb2c59447a37dd
stamander <iida.atsushi.fh@gmail.com>
2020-06-12 15:27:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'top#index' 
+  root 'products#index'
   resources :top,only: :index
   resources :message,only: :index
   resources :shop
   resources :link, only: :index
-  root 'products#index'
+  
   resources :products, except: :show
 end
