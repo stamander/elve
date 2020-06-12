47fa431ed51025de61a5e3e91d91b75454302d2b
stamander <iida.atsushi.fh@gmail.com>
2020-06-12 15:37:28 +0900

    db

--- db/migrate/20200610050227_create_images.rb
+++ db/migrate/20200610050227_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :src,null: false
+      t.string :src
+      t.references :product, foreign_key: true
      
 
       t.timestamps
--- db/migrate/20200610052123_create_exhibitions.rb
+++ db/migrate/20200610052123_create_exhibitions.rb
@@ -1,9 +1,7 @@
 class CreateExhibitions < ActiveRecord::Migration[5.2]
   def change
     create_table :exhibitions do |t|
-      t.string :name,null: false
-      t.integer :price,null: false
-      t.string :explain,null:false
+
 
 
       t.timestamps
