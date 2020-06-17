42f0af8df5f6679e002fcc448b01bf394f07c440
stamander <iida.atsushi.fh@gmail.com>
2020-06-18 07:52:45 +0900

    payjp 実装完了

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -8,13 +8,6 @@ class CardController < ApplicationController
   def new
   end
 
-  def purchase
-    Payjp.api_key = "pk_test_f64e59fe273594de973da4c0"
-    Payjp::Charge.create(
-      amount: @product.price, # 決済する値段
-      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
-      currency: 'jpy'
-    )
-  end
+
 
 end
