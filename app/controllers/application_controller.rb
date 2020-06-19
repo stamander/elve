8bc3c125f131e21ad0d949a85005300f14011278
stamander <iida.atsushi.fh@gmail.com>
2020-06-19 19:40:54 +0900

    追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     protected
   
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :kana,:adressNumber,:adress])
       end
   
 end
