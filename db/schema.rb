22aa12691cab0fcd5908eefce329ab1478aedc0f
stamander <iida.atsushi.fh@gmail.com>
2020-06-19 19:22:57 +0900

    kk

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_231440) do
+ActiveRecord::Schema.define(version: 2020_06_17_230302) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "shops_id", null: false
@@ -59,8 +59,6 @@ ActiveRecord::Schema.define(version: 2020_06_18_231440) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
