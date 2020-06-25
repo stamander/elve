00f2810e7036fcdfa0d4c7d02ba20dca120be07a
stamander <iida.atsushi.fh@gmail.com>
2020-06-25 19:56:07 +0900

    basic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -86,7 +86,7 @@ end
 
 def basic_auth
   authenticate_or_request_with_http_basic do |username, password|
-    username == 'admin' && password == '2222'
+    username =='admin' && password == '1015'
   end
 end
 
