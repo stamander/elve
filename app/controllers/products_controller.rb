d5abce98d886bb233de54abb1a924f6ede6bd2c6
stamander <iida.atsushi.fh@gmail.com>
2020-06-26 13:31:53 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -85,7 +85,8 @@ end
 
 
 def basic_auth
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+ 
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
     
   
 end
